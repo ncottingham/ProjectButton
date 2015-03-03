59deacd1f8861c55649cec6ea25fb771edc568a2
ncottingham <nicholas.cottingham@puntersouthall.com>
2015-03-03 23:13:04 +0000

    save high score

--- app/src/main/java/com/myapplication/falkon/falkon/MyActivity.java
+++ app/src/main/java/com/myapplication/falkon/falkon/MyActivity.java
@@ -1,5 +1,7 @@
 package com.myapplication.falkon.falkon;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -18,7 +20,9 @@ public class MyActivity extends Activity implements OnClickListener {
 
     Button myButton;
     TextView scoreText;
+    TextView highScoreText;
     int counter = 0;
+    int highScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +31,34 @@ public class MyActivity extends Activity implements OnClickListener {
 
         myButton = (Button)findViewById(R.id.myButton);
         scoreText = (TextView)findViewById(R.id.myTextView);
+        highScoreText = (TextView)findViewById(R.id.highScoreID);
 
         //---set on click listeners on the buttons-----
         myButton.setOnClickListener(this);
 
+
+
+        //getting preferences
+        SharedPreferences prefs = this.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        highScore = prefs.getInt("highScore", 0); //0 is the default value
+
+
+        scoreText.setText(Integer.toString(counter));
+        highScoreText.setText(Integer.toString(highScore));
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();  // Always call the superclass method first
+
+        //get shared preferences
+        //setting preferences
+        SharedPreferences prefs = this.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("highScore", highScore);
+        editor.commit();
 
     }
 
@@ -39,6 +67,10 @@ public class MyActivity extends Activity implements OnClickListener {
         if (v == myButton){
             counter++;
             scoreText.setText(Integer.toString(counter));
+            if (counter > highScore) {
+                highScore = counter;
+                highScoreText.setText(Integer.toString(highScore));
+            }
         }
 
     }
