4dd8113ff062c3c8f57d465cc8fd6cb2c5a7ff1c
ncottingham <nicholas.cottingham@puntersouthall.com>
2015-03-05 21:40:46 +0000

    added timer and temp disabled button

--- app/src/main/java/com/myapplication/falkon/falkon/MyActivity.java
+++ app/src/main/java/com/myapplication/falkon/falkon/MyActivity.java
@@ -3,6 +3,8 @@ package com.myapplication.falkon.falkon;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,8 +23,15 @@ public class MyActivity extends Activity implements OnClickListener {
     Button myButton;
     TextView scoreText;
     TextView highScoreText;
+    TextView timerValue;
     int counter = 0;
     int highScore;
+
+    long startTime = 0L;
+    Handler customHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +41,10 @@ public class MyActivity extends Activity implements OnClickListener {
         myButton = (Button)findViewById(R.id.myButton);
         scoreText = (TextView)findViewById(R.id.myTextView);
         highScoreText = (TextView)findViewById(R.id.highScoreID);
+        timerValue = (TextView) findViewById(R.id.timerValue);
 
         //---set on click listeners on the buttons-----
         myButton.setOnClickListener(this);
-
-
 
         //getting preferences
         SharedPreferences prefs = this.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
@@ -64,6 +72,9 @@ public class MyActivity extends Activity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+
         if (v == myButton){
             counter++;
             scoreText.setText(Integer.toString(counter));
@@ -74,5 +85,32 @@ public class MyActivity extends Activity implements OnClickListener {
         }
 
     }
+
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+            myButton.setEnabled(false);
+            //timeInMilliseconds = SystemClock.uptimeMillis() - startTime ;
+            timeInMilliseconds = (counter * 1000) + startTime -  SystemClock.uptimeMillis();
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000) + 1;
+            int mins = secs / 60;
+            secs = secs % 60;
+            timerValue.setText("" + mins + ":"
+                            + String.format("%02d", secs));
+            if (timeInMilliseconds <= 0) {
+                customHandler.removeCallbacks(updateTimerThread);
+                myButton.setEnabled(true);
+                timerValue.setText("" + 0 + ":"
+                        + String.format("%02d", 0));
+            }
+            else{
+                customHandler.postDelayed(this, 0);
+            }
+        }
+    };
+
 
 }
