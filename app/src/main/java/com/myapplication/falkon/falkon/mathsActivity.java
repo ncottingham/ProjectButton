09c3499190696f41f8ba4ac0db520d6bb49bc3b8
ncottingham <nicholas.cottingham@puntersouthall.com>
2015-06-02 18:58:49 +0100

    Addded menu and started maths game app

--- app/src/main/java/com/myapplication/falkon/falkon/mathsActivity.java
+++ app/src/main/java/com/myapplication/falkon/falkon/mathsActivity.java
@@ -1,28 +1,23 @@
 package com.myapplication.falkon.falkon;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.falkon.projectfalkon.R;
 
-
-public class MainActivity extends ActionBarActivity {
+public class mathsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maths);
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_maths, menu);
         return true;
     }
 
@@ -35,13 +30,6 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if (id == R.id.action_myActivity) {
-            //Intent intent = new Intent(this, MyActivity.class);
-            Intent intent = new Intent(this, MyActivity.class);
-            startActivity(intent);
             return true;
         }
 
